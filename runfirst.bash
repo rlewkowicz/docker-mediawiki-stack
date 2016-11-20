443e750886326104d5f12605686baf8ae7781228
rlewkowicz <ryan.lewkowicz@spindance.com>
2016-11-20 16:55:36 -0500

    fixed

--- runfirst.bash
+++ runfirst.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
@@ -11,7 +12,7 @@ pip install docker-compose
 useradd www-data
 rm -f $DIR/mediawiki/includes/installer/LocalSettingsGenerator.php
 \cp $DIR/distribution-files/LocalSettingsGenerator.php $DIR/distribution-files/mediawiki/includes/installer/LocalSettingsGenerator.php
-sed -i "s/\$wgDBserver = \'localhost\';/s/\$wgDBserver = \'mysql\';/g" $DIR/distribution-files/mediawiki/includes/DefaultSettings.php
+sed -i "s#wgDBserver.*localhost#wgDBserver \= \'mysql#g" $DIR/distribution-files/mediawiki/includes/DefaultSettings.php
 find $DIR/distribution-files/mediawiki -type d -exec chmod 755 {} +
 find $DIR/distribution-files/mediawiki -type f -exec chmod 644 {} +
 chown -R www-data.www-data $DIR/distribution-files/mediawiki
