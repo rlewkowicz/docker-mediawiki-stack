677870c6c0e4fa3d1a9e715e972221c6a83b2f71
rlewkowicz <ryan.lewkowicz@spindance.com>
2016-11-20 15:32:23 -0500

    init stuff that may be needed

--- runfirst.bash
+++ runfirst.bash
@@ -5,11 +5,12 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
-curl -s https://get.docker.com/ | bash
+command -v docker >/dev/null 2>&1 || { curl -s https://get.docker.com/ | bash; }
 python -m ensurepip
 pip install docker-compose
 useradd www-data
-yes | cp -rf $DIR/distribution-files/LocalSettingsGenerator.php $DIR/mediawiki/includes/installer/LocalSettingsGenerator.php
+rm -f $DIR/mediawiki/includes/installer/LocalSettingsGenerator.php
+\cp $DIR/distribution-files/LocalSettingsGenerator.php $DIR/distribution-files/mediawiki/includes/installer/LocalSettingsGenerator.php
 find $DIR/distribution-files/mediawiki -type d -exec chmod 755 {} +
 find $DIR/distribution-files/mediawiki -type f -exec chmod 644 {} +
 chown -R www-data.www-data $DIR/distribution-files/mediawiki
