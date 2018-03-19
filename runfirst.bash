8aa9f9cf27392da563916e92ee1330737e2dbaeb
ryan.lewkowicz@ge.com <ryan.lewkowicz@ge.com>
2018-03-19 13:10:06 -0500

    wip

--- runfirst.bash
+++ runfirst.bash
@@ -4,15 +4,34 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MEDIAWIKIVERSION="1.29"
 
+#system prep
 command -v docker >/dev/null 2>&1 || { curl -s https://get.docker.com/ | bash; }
-service docker start
 command -v pip >/dev/null 2>&1 || { \curl -L https://bootstrap.pypa.io/get-pip.py | python || \curl -L https://bootstrap.pypa.io/get-pip.py | python3; }
 command -v docker-compose >/dev/null 2>&1 || { pip install docker-compose; }
 getent passwd www-data >/dev/null 2>&1 || { useradd www-data; }
-rm -f $DIR/mediawiki/includes/installer/LocalSettingsGenerator.php
-\cp $DIR/distribution-files/LocalSettingsGenerator.php $DIR/distribution-files/mwcore/mediawiki/includes/installer/LocalSettingsGenerator.php
-sed -i "s#wgDBserver.*localhost#wgDBserver \= \'mysql#g" $DIR/distribution-files/mwcore/mediawiki/includes/DefaultSettings.php
+
+#Get Software
+if [[ -d "$DIR/distribution-files/mediawiki" ]]; then
+   echo "Mediawiki has already been initialized. Please remove $DIR/distribution-files/mediawiki if you would like to reninitialze the platform" 
+   echo
+   exit 1
+fi
+
+#I'm not a hugefan of this nasty bash block, but I don't want to get into crazy awkisms and
+#They don't provide a real nice way to just grab a latest release thats not nightly
+MEDIAWIKISEMVAR=$(curl -s https://releases.wikimedia.org/mediawiki/$MEDIAWIKIVERSION/ | grep -o \<.*[0-9]*\-[0-9]*\-[0-9]* \
+| grep -v sig | grep -v core | grep -v '\-rc' | grep tar.gz \
+| awk '{print $7, $6}' | sort -rn | head -1 \
+| grep -o \>mediawiki-.*.tar.gz | sed 's/>//g')
+
+wget -qO- https://releases.wikimedia.org/mediawiki/$MEDIAWIKIVERSION/$MEDIAWIKISEMVAR | tar xvz -C $DIR/distribution-files/
+
+mv $DIR/distribution-files/$(sed 's/.tar.gz//g' <(echo $MEDIAWIKISEMVAR)) $DIR/distribution-files/mediawiki
+
+
+
 find $DIR/distribution-files/mediawiki -type d -exec chmod 755 {} +
 find $DIR/distribution-files/mediawiki -type f -exec chmod 644 {} +
 chown -R www-data $DIR/distribution-files/mediawiki
